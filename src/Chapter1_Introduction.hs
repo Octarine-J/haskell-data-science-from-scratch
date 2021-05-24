c2085644d5f0f551e371294b85928ca2dd97d343
Alexander Kohan <kohan.alexander@hotmail.com>
2021-05-24 23:33:36 +0900

    ch1 utils

--- src/Chapter1_Introduction.hs
+++ src/Chapter1_Introduction.hs
@@ -1,6 +1,7 @@
 module Chapter1_Introduction where
 
 import qualified Data.Text as T
+import qualified Data.Map as M
 import Utils
 
 
@@ -9,20 +10,36 @@ insight = do
     print users
 
     friendGraph <- readCsvWith parseGraphEdge "../data/chapter_1/friend_graph.csv"
-    print friendGraph
+    let g = toFriendshipsMap friendGraph
+    print g
 
+
+type UserId = Int
+type FriendId = UserId
 
 data User = User {
-    uid  :: Int,
+    uid  :: UserId,
     name :: T.Text
 } deriving Show
 
 
+-- Parses a user, e.g. ["1", "user1"] -> User { uid = 1, name = "user1" }
 parseUser :: [T.Text] -> User
 parseUser (s_uid:s_name:_) = User {
         uid = textToInt s_uid,
         name = s_name
     }
 
-parseGraphEdge :: [T.Text] -> (Int, Int)
+-- Parses a friend graph edge, e.g. ["1", "3"] -> (1, 3)
+parseGraphEdge :: [T.Text] -> (UserId, FriendId)
 parseGraphEdge (s_from:s_to:_) = (textToInt s_from, textToInt s_to)
+
+-- Explicitly creates backward edges in a non-directional graph
+-- e.g. [(0, 1), (1, 3)] -> [(0, 1), (1, 0), (1, 3), (3, 1)]
+-- Precondition: a graph must not already contain both forward and backward edges
+makeSymmetricEdges :: [(UserId, FriendId)] -> [(UserId, FriendId)]
+makeSymmetricEdges [] = []
+makeSymmetricEdges ((from, to):xs) = (from, to):(to, from):(makeSymmetricEdges xs)
+
+toFriendshipsMap :: [(UserId, FriendId)] -> M.Map UserId [FriendId]
+toFriendshipsMap = keyValuesToMap . makeSymmetricEdges
--- src/Utils.hs
+++ src/Utils.hs
@@ -1,11 +1,14 @@
 module Utils (
         readCsv, 
         readCsvWith,
-        textToInt
+        textToInt,
+        keyValuesToMap
     ) where
 
+import qualified Data.Map as M
 import qualified Data.Text as T
 import qualified Data.Text.IO as TextIO
+import Data.List (groupBy, sort)
 
 csvSeparator :: T.Text
 csvSeparator = T.pack ","
@@ -25,3 +28,24 @@ parseCsv = map (T.splitOn csvSeparator) . T.lines
 
 textToInt :: T.Text -> Int
 textToInt = read . T.unpack
+
+-- Converts a list of key/values into a map, aggregating values
+-- e.g. [(1, "one"), (2, "two"), (1, "uno")] -> Map {1: ["one", "uno"], 2: ["two"]} 
+keyValuesToMap :: (Eq k, Ord k, Ord v) => [(k, v)] -> M.Map k [v]
+keyValuesToMap = M.fromList . groupValues
+    where
+        groupValues :: (Eq k, Ord k, Ord v) => [(k, v)] -> [(k, [v])]
+        groupValues = map foldValues . groupBy (\(x, _) (y, _) -> x == y) . sort 
+
+        -- Combines values of the same key into a list
+        -- e.g. [(1, 2), (1, 4), (1, 5)] -> (1, [2, 4, 5]) 
+        -- Precondition: the key must be the same for all values in the input list
+        foldValues :: (Eq k, Ord k, Ord v) => [(k, v)] -> (k, [v])
+        foldValues xs = foldr accumulateSameKeyValues (key, []) xs
+            where
+                key = fst . head $ xs
+
+                accumulateSameKeyValues :: (Eq k, Ord k, Ord v) => (k, v) -> (k, [v]) -> (k, [v])
+                accumulateSameKeyValues (key1, value) (key2, values)
+                    | key1 == key2 = (key1, value:values)
+                    | otherwise = error "Cannot group values for different keys."
